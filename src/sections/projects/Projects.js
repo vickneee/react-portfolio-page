0a99624bcce76eadba5653b3127fcd65fbf6b9b6
vickneee <victoria.vavulina@outlook.com>
2023-06-16 11:55:55 +0300

    Mountours project added

--- src/sections/projects/Projects.js
+++ src/sections/projects/Projects.js
@@ -8,9 +8,11 @@ import Image from "react-bootstrap/Image";
 import './Project.css';
 
 //Projects
+
+import Mountours from "../../assets/img/projects/Mountours.png";
 import PortfolioPage from "../../assets/img/projects/PortfolioPage.png";
 import RoboFriends from "../../assets/img/projects/RoboFriends.png"
-import SuperMarioMemoryGame from "../../assets/img/projects/SuperMarioMemoryGame.png";
+// import SuperMarioMemoryGame from "../../assets/img/projects/SuperMarioMemoryGame.png";
 import TicTacToe from "../../assets/img/projects/TicTacToe.png";
 
 
@@ -46,6 +48,121 @@ const ProjectTimeline = () => {
             <h1 className="pt-3 text-center font-details-b pb-3">Projects</h1>
             <Timeline theme={customTheme}>
                 <Events>
+
+                    {/* //Mountours */}
+                    <ImageEvent
+                        date="Jun 2023"
+                        className="text-center"
+                        text="Mountours"
+                        src={Mountours}
+                        alt="Mountours"
+                    >
+                        <div className="d-flex justify-content-between flex-column mt-1">
+                            <div>
+                                <Accordion className="accordion">
+                                    <Card>
+                                        <Accordion.Toggle
+                                            eventKey="0"
+                                            className="p-2 text-center accordion-main"
+                                        >
+                                            PROJECT DETAILS
+                                        </Accordion.Toggle>
+
+                                        <Accordion.Collapse eventKey="0" className="text text-left">
+                                            <Card.Body>
+                                                <strong>Description:</strong>
+                                                <br/>Want to get away from the noise of the town and be surrounded by the beauty of nature? Welcome to our Mountain Travel Tours, where we give you all the details you need to organize your upcoming mountain vacation.
+                                                <br/><br/>
+                                                <strong>Tech used:</strong>
+                                                <br/>
+                                                <ul className="d-flex content-center flex-wrap">
+                                                    <li>
+                            <span className="p-0">
+                              <Image
+                                  src={HTML5}
+                                  alt="HTML%"
+                                  className="image-style m-1 img-style"
+                              ></Image>{" "}
+                            </span>
+                                                    </li>
+                                                    <li>
+                            <span className="p-0">
+                              <Image
+                                  src={CSS3}
+                                  alt="CSS3"
+                                  className="image-style m-1 img-style"
+                              ></Image>{" "}
+                            </span>
+                                                    </li>
+                            {/*                        <li>*/}
+                            {/*<span>*/}
+                            {/*  <Image*/}
+                            {/*      src={JAVASCRIPT}*/}
+                            {/*      alt="JavaScript"*/}
+                            {/*      rounded*/}
+                            {/*      className="image-style m-1 img-style"*/}
+                            {/*  ></Image>{" "}*/}
+                            {/*</span>*/}
+                            {/*                        </li>*/}
+                            {/*                        <li>*/}
+                            {/*<span>*/}
+                            {/*  <Image*/}
+                            {/*      src={REACT}*/}
+                            {/*      alt="React"*/}
+                            {/*      className="image-style m-1 img-style"*/}
+                            {/*  ></Image>{" "}*/}
+                            {/*</span>*/}
+                            {/*                        </li>*/}
+                            {/*                        <li>*/}
+                            {/*<span >*/}
+                            {/*  <Image*/}
+                            {/*      src={REACT_BOOTSTRAP}*/}
+                            {/*      alt="React Bootstrap"*/}
+                            {/*      className="image-style m-1 img-style"*/}
+                            {/*  ></Image>{" "}*/}
+                            {/*</span>*/}
+                            {/*                        </li>*/}
+                                                    {/*<li>*/}
+                            {/*<span >*/}
+                            {/*  <Image*/}
+                            {/*      src={TAILWINDCSS}*/}
+                            {/*      alt="Tailwind CSS"*/}
+                            {/*      className="image-style m-1 img-style"*/}
+                            {/*  ></Image>{" "}*/}
+                            {/*</span>*/}
+                            {/*                        </li>*/}
+                                                    <li>
+                            <span>
+                              <Image
+                                  src={NETLIFY}
+                                  alt="Netlify"
+                                  className="image-style m-1 img-style"
+                              ></Image>{" "}
+                            </span>
+                                                    </li>
+                                                </ul>
+                                            </Card.Body>
+                                        </Accordion.Collapse>
+                                    </Card>
+                                </Accordion>
+                            </div>
+                            <div className="d-flex justify-content-between flex-nowrap text-center">
+                                <UrlButton className="url-button"
+                                           href="https://victoriavavulina.netlify.app/"
+                                           target="_blank"
+                                >
+                                    SEE LIVE
+                                </UrlButton>
+                                <UrlButton
+                                    href="https://github.com/vickneee/react-portfolio-page"
+                                    target="_blank"
+                                >
+                                    SOURCE CODE
+                                </UrlButton>
+                            </div>
+                        </div>
+                    </ImageEvent>
+
                     {/* //PortfolioPage */}
                     <ImageEvent
                         date="Jun 2023"
@@ -232,86 +349,86 @@ const ProjectTimeline = () => {
                         </div>
                     </ImageEvent>
 
-                    {/* SuperMarioMemoryGame */}
+                    {/*/!* SuperMarioMemoryGame *!/*/}
 
-                    <ImageEvent
-                        date="Apr 2023"
-                        className="text-center"
-                        text="Super Mario Memory Game"
-                        src={SuperMarioMemoryGame}
-                        alt="Super Mario Memory Game"
-                    >
-                        <div className="d-flex justify-content-between flex-column mt-1">
-                            <div>
-                                <Accordion>
-                                    <Card>
-                                        <Accordion.Toggle
-                                            eventKey="0"
-                                            className="p-2 text-center accordion-main"
-                                        >
-                                            PROJECT DETAILS
-                                        </Accordion.Toggle>
+                    {/*<ImageEvent*/}
+                    {/*    date="Apr 2023"*/}
+                    {/*    className="text-center"*/}
+                    {/*    text="Super Mario Memory Game"*/}
+                    {/*    src={SuperMarioMemoryGame}*/}
+                    {/*    alt="Super Mario Memory Game"*/}
+                    {/*>*/}
+                    {/*    <div className="d-flex justify-content-between flex-column mt-1">*/}
+                    {/*        <div>*/}
+                    {/*            <Accordion>*/}
+                    {/*                <Card>*/}
+                    {/*                    <Accordion.Toggle*/}
+                    {/*                        eventKey="0"*/}
+                    {/*                        className="p-2 text-center accordion-main"*/}
+                    {/*                    >*/}
+                    {/*                        PROJECT DETAILS*/}
+                    {/*                    </Accordion.Toggle>*/}
 
-                                        <Accordion.Collapse eventKey="0" className="text text-left">
-                                            <Card.Body>
-                                                <strong>Description:</strong>
-                                                <br/>The Super Mario Memory Game is a digital
-                                                adaptation of the classic card-matching game that revolves around the
-                                                popular video game character Mario and his friend Luigi.
-                                                <br/><br/>
-                                                <strong>Tech used:</strong>
-                                                <br/>
-                                                <ul className="d-flex content-center">
-                                                    <li>
-                            <span>
-                              <Image
-                                  src={HTML5}
-                                  alt="HTML 5"
-                                  className="image-style m-1 img-style"
-                              ></Image>{" "}
-                            </span>
-                                                    </li>
-                                                    <li>
-                            <span>
-                              <Image
-                                  src={CSS3}
-                                  alt="CSS3"
-                                  className="image-style m-1 img-style"
-                              ></Image>{" "}
-                            </span>
-                                                    </li>
-                                                    <li>
-                            <span>
-                              <Image
-                                  src={JAVASCRIPT}
-                                  alt="JavaScript"
-                                  className="image-style m-1 img-style"
-                              ></Image>{" "}
-                            </span>
-                                                    </li>
-                                                </ul>
-                                            </Card.Body>
-                                        </Accordion.Collapse>
-                                    </Card>
-                                </Accordion>
-                            </div>
-                            <div className="d-flex justify-content-between flex-nowrap text-center">
-                                <UrlButton
-                                    href="https://vickneee.github.io/super-mario-memory-game/"
-                                    target="_blank"
-                                >
-                                    SEE LIVE
-                                </UrlButton>
-                                <UrlButton
-                                    href="https://github.com/vickneee/super-mario-memory-game"
-                                    target="_blank"
-                                >
-                                    SOURCE CODE
-                                </UrlButton>
+                    {/*                    <Accordion.Collapse eventKey="0" className="text text-left">*/}
+                    {/*                        <Card.Body>*/}
+                    {/*                            <strong>Description:</strong>*/}
+                    {/*                            <br/>The Super Mario Memory Game is a digital*/}
+                    {/*                            adaptation of the classic card-matching game that revolves around the*/}
+                    {/*                            popular video game character Mario and his friend Luigi.*/}
+                    {/*                            <br/><br/>*/}
+                    {/*                            <strong>Tech used:</strong>*/}
+                    {/*                            <br/>*/}
+                    {/*                            <ul className="d-flex content-center">*/}
+                    {/*                                <li>*/}
+                    {/*        <span>*/}
+                    {/*          <Image*/}
+                    {/*              src={HTML5}*/}
+                    {/*              alt="HTML 5"*/}
+                    {/*              className="image-style m-1 img-style"*/}
+                    {/*          ></Image>{" "}*/}
+                    {/*        </span>*/}
+                    {/*                                </li>*/}
+                    {/*                                <li>*/}
+                    {/*        <span>*/}
+                    {/*          <Image*/}
+                    {/*              src={CSS3}*/}
+                    {/*              alt="CSS3"*/}
+                    {/*              className="image-style m-1 img-style"*/}
+                    {/*          ></Image>{" "}*/}
+                    {/*        </span>*/}
+                    {/*                                </li>*/}
+                    {/*                                <li>*/}
+                    {/*        <span>*/}
+                    {/*          <Image*/}
+                    {/*              src={JAVASCRIPT}*/}
+                    {/*              alt="JavaScript"*/}
+                    {/*              className="image-style m-1 img-style"*/}
+                    {/*          ></Image>{" "}*/}
+                    {/*        </span>*/}
+                    {/*                                </li>*/}
+                    {/*                            </ul>*/}
+                    {/*                        </Card.Body>*/}
+                    {/*                    </Accordion.Collapse>*/}
+                    {/*                </Card>*/}
+                    {/*            </Accordion>*/}
+                    {/*        </div>*/}
+                    {/*        <div className="d-flex justify-content-between flex-nowrap text-center">*/}
+                    {/*            <UrlButton*/}
+                    {/*                href="https://vickneee.github.io/super-mario-memory-game/"*/}
+                    {/*                target="_blank"*/}
+                    {/*            >*/}
+                    {/*                SEE LIVE*/}
+                    {/*            </UrlButton>*/}
+                    {/*            <UrlButton*/}
+                    {/*                href="https://github.com/vickneee/super-mario-memory-game"*/}
+                    {/*                target="_blank"*/}
+                    {/*            >*/}
+                    {/*                SOURCE CODE*/}
+                    {/*            </UrlButton>*/}
 
-                            </div>
-                        </div>
-                    </ImageEvent>
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</ImageEvent>*/}
 
                     {/* //TicTacToe */}
                         <ImageEvent
