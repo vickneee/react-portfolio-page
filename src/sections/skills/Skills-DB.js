dbb244ce81c119af4cebcc34fc0d3aed07031d56
vickneee <victoria.vavulina@outlook.com>
2023-08-26 11:48:39 +0300

    Skills update

--- src/sections/skills/Skills-DB.js
+++ src/sections/skills/Skills-DB.js
@@ -19,6 +19,7 @@ import CPLUSPLUS from "../../assets/img/skills/cplusplus_1.png"
 import NODEJS from "../../assets/img/skills/node-js.png"
 // import EXPRESS from "../../assets/img/skills/ExpressJS-Dark.svg"
 import MYSQL from "../../assets/img/skills/my-sql.png"
+import MONGODB from "../../assets/img/skills/mongo-db.svg"
 // import FIREBASE from "../../assets/img/skills/firebase-icon.svg"
 import NETLIFY from "../../assets/img/skills/netlify.svg"
 // import HEROKU from "../../assets/img/skills/heroku.png";
@@ -151,6 +152,12 @@ export const skills = {
         imgAltText: "MySQL",
         imgSrc: MYSQL,
         skillName: "MySQL",
+      },
+      {
+        link: "https://www.mongodb.com/",
+        imgAltText: "MongoDB",
+        imgSrc: MONGODB,
+        skillName: "MongoDB",
       },
       // {
       //   link: "https://firebase.google.com/",
