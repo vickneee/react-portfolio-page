7d4580d54ef95b135cc7bb10ce29f4e4bba585dc
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-02-12 00:45:57 +0200

    Skill updates

--- src/sections/skills/Skills-DB.js
+++ src/sections/skills/Skills-DB.js
@@ -1,16 +1,13 @@
 import SUBLIMETEXT from "../../assets/img/skills/sublime-text.png"
-import VISUALSTUDIOCODE from "../../assets/img/skills/visual-studio-code.png"
+import VISUAL_STUDIO_CODE from "../../assets/img/skills/visual-studio-code.png"
 import WEBSTORM from "../../assets/img/skills/webstorm.png"
 import PYCHARM from "../../assets/img/skills/pycharm.png"
-// import RIDER from "../../assets/img/skills/rider.jpeg"
-// import WORDPRESS from "../../assets/img/skills/Wordpress.svg"
 // import FIGMA from "../../assets/img/skills/figma-logo.png"
 import GIT from "../../assets/img/skills/git-icon.svg";
 import GITHUB_PAGES from "../../assets/img/skills/github.svg";
 import MARKDOWN from '../../assets/img/skills/markdown.svg'
 import HTML5 from "../../assets/img/skills/HTML5.png";
 import CSS3 from "../../assets/img/skills/CSS3.png";
-// import SASS from "../../assets/img/skills/Sass.svg";
 // import JAVASCRIPT from "../../assets/img/skills/JS.jpeg";
 // import TYPESCRIPT from "../../assets/img/skills/typescript.svg";
 import REACT from "../../assets/img/skills/react.png";
@@ -18,7 +15,7 @@ import REACT_BOOTSTRAP from "../../assets/img/skills/react-bootstrap.svg";
 // import MATERIALUI from "../../assets/img/skills/material-ui.svg";
 import TAILWINDCSS from "../../assets/img/skills/tailwindcss.svg"
 // import REDUX from "../../assets/img/skills/redux.svg";
-// import REACTNATIVE from "../../assets/img/skills/react-native.png";
+// import REACT_NATIVE from "../../assets/img/skills/react-native.png";
 // import EXPO from "../../assets/img/skills/Expo.png";
 import PYTHON from "../../assets/img/skills/python.svg"
 // import CPLUSPLUS from "../../assets/img/skills/cplusplus-logo.png"
@@ -35,204 +32,185 @@ import NETLIFY from "../../assets/img/skills/netlify.svg"
 export const skills = {
 
     skillsList: [
-      {
-        link: "https://www.sublimetext.com/",
-        imgAltText: "Sublime Text",
-        imgSrc: SUBLIMETEXT,
-        skillName: "Sublime Text",
-      },
-      {
-        link: "https://code.visualstudio.com/",
-        imgAltText: "Visual Studio Code",
-        imgSrc: VISUALSTUDIOCODE,
-        skillName: "Visual Studio Code",
-      },
-      {
-        link: "https://www.jetbrains.com/webstorm/",
-        imgAltText: "WebStorm",
-        imgSrc: WEBSTORM,
-        skillName: "WebStorm",
-      },
-      {
-        link: "https://www.jetbrains.com/pycharm/",
-        imgAltText: "PyCharm",
-        imgSrc: PYCHARM,
-        skillName: "PyCharm",
-      },
-      // {
-      //   link: "https://www.jetbrains.com/rider/",
-      //   imgAltText: "Rider",
-      //   imgSrc: RIDER,
-      //   skillName: "Rider",
-      // },
-      // {
-      //   link: "https://www.wordpress.com/",
-      //   imgAltText: "WordPress",
-      //   imgSrc: WORDPRESS,
-      //   skillName: "WordPress",
-      // },
-      // {
-      //   link: "https://www.figma.com/",
-      //   imgAltText: "Figma",
-      //   imgSrc: FIGMA,
-      //   skillName: "Figma",
-      // },
-      {
-        link: "https://git-scm.com/",
-        imgAltText: "GIT",
-        imgSrc: GIT,
-        skillName: "GIT",
-      },
-      {
-        link: "https://github.com/",
-        imgAltText: "GitHub Pages",
-        imgSrc: GITHUB_PAGES,
-        skillName: "GitHub Pages",
-      },
-      {
-        link: "https://www.markdownguide.org/",
-        imgAltText: "Markdown",
-        imgSrc: MARKDOWN,
-        skillName: "Markdown",
-      },
-      {
-        link: "https://www.w3schools.com/html/html_intro.asp",
-        imgAltText: "HTML",
-        imgSrc: HTML5,
-        skillName: "HTML",
-      },
-      {
-        link: "https://www.w3schools.com/Css/css_intro.asp",
-        imgAltText: "CSS",
-        imgSrc: CSS3,
-        skillName: "CSS",
-      },
-      // {
-      //   link: "https://sass-lang.com/",
-      //   imgAltText: "Sass",
-      //   imgSrc: SASS,
-      //   skillName: "Sass",
-      // },
-      // {
-      //   link: "https://www.w3schools.com/js/js_intro.asp",
-      //   imgAltText: "JavaScript",
-      //   imgSrc: JAVASCRIPT,
-      //   skillName: "JavaScript",
-      // },
-      // {
-      //   link: "https://www.typescriptlang.org/",
-      //   imgAltText: "TypeScript",
-      //   imgSrc: TYPESCRIPT,
-      //   skillName: "TypeScript",
-      // },
-      {
-        link: "https://reactjs.org/",
-        imgAltText: "React JS",
-        imgSrc: REACT,
-        skillName: "React JS",
-      },
-      {
-        link: "https://react-bootstrap.github.io/",
-        imgAltText: "React Bootstrap",
-        imgSrc: REACT_BOOTSTRAP,
-        skillName: "React Bootstrap",
-      },
-      // {
-      //   link: "https://material-ui.com/",
-      //   imgAltText: "Material-UI",
-      //   imgSrc: MATERIALUI,
-      //   skillName: "Material-UI",
-      // },
-      {
-        link: "https://tailwindcss.com/",
-        imgAltText: "Tailwind CSS",
-        imgSrc: TAILWINDCSS,
-        skillName: "Tailwind CSS",
-      },
-      // {
-      //   link: "https://reactnative.dev/",
-      //   imgAltText: "React Native",
-      //   imgSrc: REACTNATIVE,
-      //   skillName: "React NAtive",
-      // },
-      // {
-      //   link: "https://expo.dev/",
-      //   imgAltText: "Expo",
-      //   imgSrc: EXPO,
-      //   skillName: "Expo",
-      // },
-      // {
-      //   link: "https://redux.js.org/",
-      //   imgAltText: "Redux",
-      //   imgSrc: REDUX,
-      //   skillName: "Redux",
-      // },
-      {
-        link: "https://www.python.org/",
-        imgAltText: "Python",
-        imgSrc: PYTHON,
-        skillName: "Python",
-      },
-      // {
-      //   link: "https://cplusplus.com/",
-      //   imgAltText: "C++",
-      //   imgSrc: CPLUSPLUS,
-      //   skillName: "C++",
-      // },
-      // {
-      //   link: "https://dotnet.microsoft.com/en-us/languages/csharp",
-      //   imgAltText: "C#",
-      //   imgSrc: CSHARP,
-      //   skillName: "C#",
-      // },
-      {
-        link: "https://nodejs.org/",
-        imgAltText: "Node.js",
-        imgSrc: NODEJS,
-        skillName: "Node.js",
-      },
-      // {
-      //   link: "https://expressjs.com/",
-      //   imgAltText: "Express.js",
-      //   imgSrc: EXPRESSJS,
-      //   skillName: "Express.js",
-      // },
-      // {
-      //   link: "https://nextjs.org/",
-      //   imgAltText: "Next.js",
-      //   imgSrc: NEXTJS,
-      //   skillName: "Next.js",
-      // },
-      // {
-      //   link: "https://www.mongodb.com/",
-      //   imgAltText: "MongoDB",
-      //   imgSrc: MONGODB,
-      //   skillName: "MongoDB",
-      // },
-      {
-        link: "https://mysql.com/",
-        imgAltText: "MySQL",
-        imgSrc: MYSQL,
-        skillName: "MySQL",
-      },
-      // {
-      //   link: "https://firebase.google.com/",
-      //   imgAltText: "Firebase Database",
-      //   imgSrc: FIREBASE,
-      //   skillName: "Firebase Database",
-      // },
-      {
-        link: "https://www.netlify.com/",
-        imgAltText: "Netlify",
-        imgSrc: NETLIFY,
-        skillName: "Netlify",
-      },
-      // {
-      //   link: "https://www.heroku.com/",
-      //   imgAltText: "Heroku",
-      //   imgSrc: HEROKU,
-      //   skillName: "Heroku",
-      // },
+        {
+            link: "https://www.sublimetext.com/",
+            imgAltText: "Sublime Text",
+            imgSrc: SUBLIMETEXT,
+            skillName: "Sublime Text",
+        },
+        {
+            link: "https://code.visualstudio.com/",
+            imgAltText: "Visual Studio Code",
+            imgSrc: VISUAL_STUDIO_CODE,
+            skillName: "Visual Studio Code",
+        },
+        {
+            link: "https://www.jetbrains.com/webstorm/",
+            imgAltText: "WebStorm",
+            imgSrc: WEBSTORM,
+            skillName: "WebStorm",
+        },
+        {
+            link: "https://www.jetbrains.com/pycharm/",
+            imgAltText: "PyCharm",
+            imgSrc: PYCHARM,
+            skillName: "PyCharm",
+        },
+        // {
+        //   link: "https://www.figma.com/",
+        //   imgAltText: "Figma",
+        //   imgSrc: FIGMA,
+        //   skillName: "Figma",
+        // },
+        {
+            link: "https://git-scm.com/",
+            imgAltText: "GIT",
+            imgSrc: GIT,
+            skillName: "GIT",
+        },
+        {
+            link: "https://github.com/",
+            imgAltText: "GitHub Pages",
+            imgSrc: GITHUB_PAGES,
+            skillName: "GitHub Pages",
+        },
+        {
+            link: "https://www.markdownguide.org/",
+            imgAltText: "Markdown",
+            imgSrc: MARKDOWN,
+            skillName: "Markdown",
+        },
+        {
+            link: "https://www.w3schools.com/html/html_intro.asp",
+            imgAltText: "HTML",
+            imgSrc: HTML5,
+            skillName: "HTML",
+        },
+        {
+            link: "https://www.w3schools.com/Css/css_intro.asp",
+            imgAltText: "CSS",
+            imgSrc: CSS3,
+            skillName: "CSS",
+        },
+        // {
+        //   link: "https://www.w3schools.com/js/js_intro.asp",
+        //   imgAltText: "JavaScript",
+        //   imgSrc: JAVASCRIPT,
+        //   skillName: "JavaScript",
+        // },
+        // {
+        //   link: "https://www.typescriptlang.org/",
+        //   imgAltText: "TypeScript",
+        //   imgSrc: TYPESCRIPT,
+        //   skillName: "TypeScript",
+        // },
+        {
+            link: "https://reactjs.org/",
+            imgAltText: "React JS",
+            imgSrc: REACT,
+            skillName: "React JS",
+        },
+        {
+            link: "https://react-bootstrap.github.io/",
+            imgAltText: "React Bootstrap",
+            imgSrc: REACT_BOOTSTRAP,
+            skillName: "React Bootstrap",
+        },
+        // {
+        //   link: "https://material-ui.com/",
+        //   imgAltText: "Material-UI",
+        //   imgSrc: MATERIALUI,
+        //   skillName: "Material-UI",
+        // },
+        {
+            link: "https://tailwindcss.com/",
+            imgAltText: "Tailwind CSS",
+            imgSrc: TAILWINDCSS,
+            skillName: "Tailwind CSS",
+        },
+        // {
+        //   link: "https://reactnative.dev/",
+        //   imgAltText: "React Native",
+        //   imgSrc: REACT_NATIVE,
+        //   skillName: "React NAtive",
+        // },
+        // {
+        //   link: "https://expo.dev/",
+        //   imgAltText: "Expo",
+        //   imgSrc: EXPO,
+        //   skillName: "Expo",
+        // },
+        // {
+        //   link: "https://redux.js.org/",
+        //   imgAltText: "Redux",
+        //   imgSrc: REDUX,
+        //   skillName: "Redux",
+        // },
+        {
+            link: "https://www.python.org/",
+            imgAltText: "Python",
+            imgSrc: PYTHON,
+            skillName: "Python",
+        },
+        // {
+        //   link: "https://cplusplus.com/",
+        //   imgAltText: "C++",
+        //   imgSrc: CPLUSPLUS,
+        //   skillName: "C++",
+        // },
+        // {
+        //   link: "https://dotnet.microsoft.com/en-us/languages/csharp",
+        //   imgAltText: "C#",
+        //   imgSrc: CSHARP,
+        //   skillName: "C#",
+        // },
+        {
+            link: "https://nodejs.org/",
+            imgAltText: "Node.js",
+            imgSrc: NODEJS,
+            skillName: "Node.js",
+        },
+        // {
+        //   link: "https://expressjs.com/",
+        //   imgAltText: "Express.js",
+        //   imgSrc: EXPRESSJS,
+        //   skillName: "Express.js",
+        // },
+        // {
+        //   link: "https://nextjs.org/",
+        //   imgAltText: "Next.js",
+        //   imgSrc: NEXTJS,
+        //   skillName: "Next.js",
+        // },
+        // {
+        //   link: "https://www.mongodb.com/",
+        //   imgAltText: "MongoDB",
+        //   imgSrc: MONGODB,
+        //   skillName: "MongoDB",
+        // },
+        {
+            link: "https://mysql.com/",
+            imgAltText: "MySQL",
+            imgSrc: MYSQL,
+            skillName: "MySQL",
+        },
+        // {
+        //   link: "https://firebase.google.com/",
+        //   imgAltText: "Firebase Database",
+        //   imgSrc: FIREBASE,
+        //   skillName: "Firebase Database",
+        // },
+        {
+            link: "https://www.netlify.com/",
+            imgAltText: "Netlify",
+            imgSrc: NETLIFY,
+            skillName: "Netlify",
+        },
+        // {
+        //   link: "https://www.heroku.com/",
+        //   imgAltText: "Heroku",
+        //   imgSrc: HEROKU,
+        //   skillName: "Heroku",
+        // },
     ],
-
-  };
+};
