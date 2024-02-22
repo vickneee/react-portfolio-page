9c280a1e8f6fb766688fa472afb09e60db4d8153
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-02-22 18:25:48 +0200

    Update TitleMessage.json

--- src/components/title-message/TitleMessage.js
+++ src/components/title-message/TitleMessage.js
@@ -1,34 +1,7 @@
 import React from "react";
 import Typewriter from 'typewriter-effect';
-import styled from "styled-components";
-import './TitleMessage.css'
-
-const MyTitleMessage = styled.h1`
-  position: absolute;
-  width: 100%;
-  top: 22rem;
-  z-index: 1;
-  text-align: center;
-
-  strong {
-    font-size: 1.25em;
-  }
-
-  div {
-    color: ${props => props.theme};
-    text-shadow: 0 2px 5px rgba(0, 0, 0, 0.4);
-    font-weight: 100;
-
-    .main {
-      font-size: 50px;
-    }
-
-    .sub {
-      font-size: 27px;
-      letter-spacing: 2px;
-    }
-  }
-`;
+import MyTitleMessage from './MyTitleMessage';
+import './TitleMessage.css';
 
 const TitleMessage = () => (
     <MyTitleMessage>
@@ -38,8 +11,8 @@ const TitleMessage = () => (
                     Hi, I am
                     <br/>
                     <span className="my-name">
-              <strong>Victoria Vavulina</strong>
-            </span>
+            <strong>Victoria Vavulina</strong>
+          </span>
                 </div>
                 <div className="sub">
                     <Typewriter
