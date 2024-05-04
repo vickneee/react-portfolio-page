92701e312341c697615ec16b5c36fc9d40d1e8ae
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-05-04 12:29:27 +0300

    Footer Update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,9 +4,8 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <div>
-            <footer className="py-4 text-center footer-style">Made by Victoria
-                Vavulina <span>©&nbsp;</span>️2023</footer>
-    </div>
+            <footer className="py-4 text-center footer-style">Made by Victoria Vavulina <span>©&nbsp;</span>️2023</footer>
+    	</div>
     );
 };
 
